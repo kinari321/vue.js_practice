83696c3870bdf8087595f2b375f07ed58a0e4a96
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-08-10 16:52:11 +0900

    Udemy 82

--- udemy-vuejs/src/App.vue
+++ udemy-vuejs/src/App.vue
@@ -1,3 +1,16 @@
 <template>
+<div>
+   <LikeHeader></LikeHeader>
    <LikeNumber></LikeNumber>
+</div>
 </template>
+
+<script>
+import LikeHeader from "./LikeHeader.vue";
+
+export default {
+  components: {
+    LikeHeader: LikeHeader
+  }
+};
+</script>
---
+++ udemy-vuejs/src/LikeHeader.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>タイトル</h1>
+</template>
