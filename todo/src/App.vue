fb5d3580aafa115c92c04ace02f7b9a166b4c283
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-08-11 14:52:07 +0900

    3-4 リスト表示

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,38 +1,27 @@
 <template>
-  <div >
-  <input v-bind:value="changeResult" v-on:change="onChange($event)" placeholder="change"> <!-- v-bind:valueに初期値 -->
-    {{ changeResult }}
-  <br>
-  <input v-bind:value="inputResult" @input="onInput($event)" placeholder="input">
-    {{ inputResult }}
-  <br>
-  <input v-bind:value="keyupResult" @keyup="onKeyup($event)" placeholder="keyup">
-    {{ keyupResult }}
-  <br>
-  <input v-model="vmodelResult" placeholder="v-model">
-    {{ vmodelResult }}
-  </div>
+  <ul>
+    <li v-for="todo in todos" v-bind:key="todo.id"> <!-- v-forでリスト表示 -->
+      {{ todo.text }}
+      <button v-on:click="deleteTodo(todo.id)">x</button>
+    </li>
+  </ul>
+
 </template>
 
 <script>
 export default {
   data () {
     return {
-      changeResult: "change default value", // 初期値
-      inputResult: "input default value",
-      keyupResult: "keyup default value",
-      vmodelResult: "vmodel default value"
+      todos: [
+        {id: "1", text: "todo 1"},
+        {id: "2", text: "todo 2"},
+        {id: "3", text: "todo 3"}
+      ]
     }
   },
   methods: {
-    onChange(e) { // 引数（e）でイベントオブジェクトを参照可能
-      this.changeResult = e.target.value;
-    },
-    onInput(e) {
-      this.inputResult = e.target.value;
-    },
-    onKeyup(e) {
-      this.keyupResult = e.target.value;
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id != id);
     }
   }
 }
