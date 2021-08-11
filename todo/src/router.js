1100e0f10b74d15f29bc7e4a9c05bd912f08f86d
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-08-11 16:23:18 +0900

    3-7ルーティング2 Hashモード

--- todo/src/router.js
+++ todo/src/router.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import PageOne from './components/PageOne' // ルートの定義
-import PageTwo from './components/PageTwo' // ルートの定義
+import PageOne from './components/PageOne'
+import PageTwo from './components/PageTwo'
 
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history', // historyモード
   base: process.env.BASE_URL,
-  routes: [ // 配列でルートの定義
+  routes: [
     {
       path: '/',
       component: PageOne
