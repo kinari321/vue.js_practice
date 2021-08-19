d91bc8dd75d274344d3dff26f669492a82803fe1
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-08-19 10:53:01 +0900

    雛形を作成

--- udemy-vuejs-axios/src/App.vue
+++ udemy-vuejs-axios/src/App.vue
@@ -4,9 +4,36 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <h3>掲示板に投稿する</h3>
+      <label for="name">ニックネーム:</label>
+      <input
+        id="name"
+        type="text"
+        v-model="name"
+      >
+      <br><br>
+      <button @click="createComment">コメントをサーバーに送る</button>
+      <label for="comment">コメント：</label>
+      <textarea
+        id="comment"
+        v-model="comment"
+      >
+      </textarea>
+      <h2>掲示板</h2>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      comment: '',
+    };
+  },
+};
+</script>
 
 <style>
 #app {
