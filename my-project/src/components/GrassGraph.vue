8330d292fadeff93f56a22bbf4465ef47c2c24fe
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-09-25 15:11:40 +0900

    iconの追加

--- my-project/src/components/GrassGraph.vue
+++ my-project/src/components/GrassGraph.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="github-link">
       <i class="fab fa-github-square"></i>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
       <a href="https://github.com/kinari321" target="_blank">kinari</a>
     </div>
     <a href="https://github.com/kinari321" target="_blank">
@@ -9,3 +10,10 @@
     </a>
   </div>
 </template>
+
+<style>
+.github-link {
+  font-size: 14px;
+  margin-bottom: 10px;
+}
+</style>
