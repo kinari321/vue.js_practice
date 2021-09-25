e0613d97759070161369117ade2fa8a996562899
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-09-25 14:58:05 +0900

    GrassGraphの追加

---
+++ my-project/src/components/GrassGraph.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="github-link">
+      <i class="fab fa-github-square"></i>
+      <a href="https://github.com/kinari321" target="_blank">kinari</a>
+    </div>
+    <a href="https://github.com/kinari321" target="_blank">
+      <img src="https://grass-graph.appspot.com/images/kinari321.png">
+    </a>
+  </div>
+</template>
